acff86b1067371528165317f243ebf0637f02913
AykoPoel <ayko.poel@gmail.com>
2015-04-01 13:28:03 +0200

    Update gpt_ext4.sh

--- blocks/filesystem/gpt_ext4.sh
+++ blocks/filesystem/gpt_ext4.sh
@@ -8,8 +8,9 @@ _drivequery;
 
 BOOT_DRIVE=$INSTALL_DRIVE # expected format /dev/sda
 PARTITION_EFI_BOOT=1
-LABEL_BOOT_EFI=bootefi
+LABEL_BOOT_EFI=efi
 LABEL_SWAP=swap
+LABEL_HOME=home
 LABEL_ROOT=root
 MOUNT_PATH=/mnt
 EFI_SYSTEM_PARTITION=/boot/efi
@@ -32,29 +33,37 @@ sgdisk -a 2048 -o ${INSTALL_DRIVE} # new gpt disk 2048 alignment
 
 # create partitions
 sgdisk -n ${PARTITION_EFI_BOOT}:0:+200M ${INSTALL_DRIVE} # (UEFI BOOT), default start block, 200MB
-sgdisk -n ${PARTITION_SWAP}:0:+2G ${INSTALL_DRIVE} # (SWAP), default start block, 2GB
-sgdisk -n ${PARTITION_ROOT}:0:0 ${INSTALL_DRIVE}   # (LUKS), default start, remaining space
+#sgdisk -n ${PARTITION_SWAP}:0:+2G ${INSTALL_DRIVE} # (SWAP), default start block, 2GB
+sgdisk -n ${PARTITION_ROOT}:0:+50G ${INSTALL_DRIVE}   # (LUKS), default start, remaining space
+sgdisk -n ${PARTITION_HOME}:0:0 ${INSTALL_DRIVE}   # (LUKS), default start, remaining space
+
+#sgdisk -n ${PARTITION_SWAP}:0:+2G ${INSTALL_DRIVE} # (SWAP), default start block, 2GB
 
 # set partition types
 sgdisk -t ${PARTITION_EFI_BOOT}:ef00 ${INSTALL_DRIVE}
-sgdisk -t ${PARTITION_SWAP}:8200 ${INSTALL_DRIVE}
+#sgdisk -t ${PARTITION_SWAP}:8200 ${INSTALL_DRIVE}
 sgdisk -t ${PARTITION_ROOT}:8300 ${INSTALL_DRIVE}
+sgdisk -t ${PARTITION_HOME}:8300 ${INSTALL_DRIVE}
 
 # label partitions
 sgdisk -c ${PARTITION_EFI_BOOT}:"${LABEL_BOOT_EFI}" ${INSTALL_DRIVE}
-sgdisk -c ${PARTITION_SWAP}:"${LABEL_SWAP}" ${INSTALL_DRIVE}
+#sgdisk -c ${PARTITION_SWAP}:"${LABEL_SWAP}" ${INSTALL_DRIVE}
 sgdisk -c ${PARTITION_ROOT}:"${LABEL_ROOT}" ${INSTALL_DRIVE}
+sgdisk -c ${PARTITION_HOME}:"${LABEL_HOME}" ${INSTALL_DRIVE}
+
 
 # make filesystems
 mkfs.vfat ${INSTALL_DRIVE}${PARTITION_EFI_BOOT}
-mkswap ${INSTALL_DRIVE}${PARTITION_SWAP}
-swapon ${INSTALL_DRIVE}${PARTITION_SWAP}
+#mkswap ${INSTALL_DRIVE}${PARTITION_SWAP}
+#swapon ${INSTALL_DRIVE}${PARTITION_SWAP}
 mkfs.ext4 ${INSTALL_DRIVE}${PARTITION_ROOT}
+mkfs.ext4 ${INSTALL_DRIVE}${PARTITION_HOME}
 # mkswap /dev/sda2
 
 # mount target
 mkdir -p ${MOUNT_PATH}
 mount ${INSTALL_DRIVE}${PARTITION_ROOT} ${MOUNT_PATH}
+mount ${INSTALL_DRIVE}${PARTITION_HOME} ${MOUNT_PATH}
 mkdir -p ${MOUNT_PATH}${EFI_SYSTEM_PARTITION}
 mount -t vfat ${INSTALL_DRIVE}${PARTITION_EFI_BOOT} ${MOUNT_PATH}${EFI_SYSTEM_PARTITION}
 }
@@ -70,6 +79,7 @@ tmpfs						/tmp		tmpfs	nodev,nosuid				0	0
 /dev/disk/by-partlabel/${LABEL_BOOT_EFI}		$EFI_SYSTEM_PARTITION	vfat	rw,relatime,discard,fmask=0022,dmask=0022,codepage=437,iocharset=iso8859-1,shortname=mixed,errors=remount-ro	0 2
 /dev/disk/by-partlabel/${LABEL_SWAP}				none		swap	defaults,discard			0	0
 /dev/disk/by-partlabel/${LABEL_ROOT}				/      		ext4	rw,relatime,data=ordered,discard	0	1
+/dev/disk/by-partlabel/${LABEL_HOME}				/      		ext4	rw,relatime,data=ordered,discard	0	2
 FSTAB_EOF
 }
 
